7dd8ae735f34b1e5967c2efd1401fae26bb3143b
Kawã Sousa <kawasousacontato@gmail.com>
2025-01-28 19:25:58 -0300

    :recycle: refactor: Adiciona handler de erro de conexão com o servidor

--- server.ts
+++ server.ts
@@ -1,7 +1,13 @@
 import app from "./app";
+import prisma from "./prisma/client";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    `Server running on port ${PORT}.`
+prisma.$connect().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}.`);
+    })
+}
+).catch((err: any) => {
+    console.error(`Error when trying to connect to the server: ${err}.`);
 })

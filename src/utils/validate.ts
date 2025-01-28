a981fc41c37ac53ee1cff30d274ff039323a1924
Kawã Sousa <kawasousacontato@gmail.com>
2025-01-28 19:28:30 -0300

    :sparkles: feat: Adiciona função de validação dos parâmetros do request e atualiza a função inicial para validação do body do request

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,10 +1,21 @@
 import { ZodSchema } from 'zod'
 import { Request, Response, NextFunction } from 'express'
 
-export function validate(schema: ZodSchema) {
+export function validateBody(schema: ZodSchema) {
     return function(req: Request, res: Response, next: NextFunction) {
         try {
             schema.parse(req.body);
+            next();
+        } catch (err: any) {
+            res.status(400).json({ error: err.errors })
+        }
+    }
+}
+
+export function validateParams(schema: ZodSchema) {
+    return function(req: Request, res: Response, next: NextFunction) {
+        try {
+            schema.parse(req.params);
             next();
         } catch (err: any) {
             res.status(400).json({ error: err.errors })

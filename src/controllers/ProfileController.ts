40bcb239c5386b7647bfd8f8eaa5b1f7c766a6e5
Kawã Sousa <kawasousacontato@gmail.com>
2025-02-02 08:07:00 -0300

    :sparkles: feat: Adiciona método de criação e atualiza método de busca de perfil no Profile Controller

--- src/controllers/ProfileController.ts
+++ src/controllers/ProfileController.ts
@@ -1,34 +1,27 @@
 import { Request, Response } from "express";
 import ProfileService from "../services/ProfileService";
+import { ProfileDTO } from "../models/ProfileDTO";
 
-export default class ProfileController{
+export default class ProfileController {
     private profileService: ProfileService;
 
-    constructor(){
+    constructor() {
         this.profileService = new ProfileService();
     }
 
-    public async getAllProfiles(req: Request, res: Response): Promise<void>{
-        const response = await this.profileService.findAllProfiles();
-        res.status(200).json(response);
+    public async getAllProfiles(req: Request, res: Response): Promise<void> {
+        const profiles: ProfileDTO[] = await this.profileService.findAllProfiles();
+        res.status(200).json({profiles});
     }
 
-    public async createProfile(req: Request, res: Response){
-        const {
-            nickname,
-            email,
-            photo_url,
-            password,
-            isActivated,
-            isAdvanced
-        } = req.body;
+    public async deleteProfile(req: Request, res: Response): Promise<void> {
+        const { id } = req.params;
+        const user = this.profileService.deleteProfile(id);
 
-        try {
-            const profile = await this.profileService.createProfile(nickname, email, photo_url, password, isActivated, isAdvanced);
-            res.status(201).json(profile);
-
-        } catch (error: any) {
-            res.status(400).json({message: `An user with this ${error.meta.target} already exists.`})
+        if(!user){
+            res.status(204).json({status: "failure"});
         }
+
+        res.status(204).json({status: "sucess"});
     }
 }

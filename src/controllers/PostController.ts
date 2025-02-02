ad264e662d14df9624cbbf865046fb1e46b19f43
Kawã Sousa <kawasousacontato@gmail.com>
2025-02-02 07:58:45 -0300

    :lock: feat: Adiciona verificações às rotas de criação e exclusão de posts no Post Controller

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -14,26 +14,37 @@ export default class PostController {
     }
 
     public async createPost(req: Request, res: Response): Promise<void> {
-        const { isAdvanced, content, profileId } = req.body;
+        const { content, username } = req.body;
+
+        if (!content || !username) {
+            res.status(404).json({ error: "Required fields missing" })
+            return;
+        }
 
         try {
-            const post = await this.postService.createPost(content, profileId);
-            res.status(201).json(post);
+            const postDTO = await this.postService.createPost(content, username);
 
-        } catch (err: any) {
-            res.status(400).json({ error: err.errors })
+            res.status(201).json(postDTO);
+        } catch (error: any) {
+            console.log(error);
+            res.status(404).json({ error: error.errors })
         }
     }
 
-    public async deletePost(req: Request, res: Response) {
+    public async deletePost(req: Request, res: Response): Promise<void> {
         const { id } = req.params;
+        const { requestingUsername } = req.body;
 
         try {
-            const post = await this.postService.deletePost(id);
-            res.status(200).json(post);
+            if (!id || !requestingUsername) throw new Error('post id and requesting username is required to delete a post');
 
-        } catch (err: any) {
-            res.status(400).json({ error: err.errors })
+            await this.postService.deletePost(id, requestingUsername);
+            res.status(204).json({status: 'sucess'});
+
+        } catch (error: any) {
+            console.log(error);
+            
+            res.status(400).json({ status: 'failure', error: error.errors })
         }
     }
 }

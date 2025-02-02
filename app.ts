b487bd5b76cd2d4e7cc33178747b585edba1a937
Kawã Sousa <kawasousacontato@gmail.com>
2025-02-02 07:51:26 -0300

    :sparkles: feat: Adiciona configuração de cors e cookie parser. Adiciona rota de base autenticação

--- app.ts
+++ app.ts
@@ -2,14 +2,30 @@ import express from 'express'
 import dotenv from 'dotenv'
 import PostRouter from "./src/routes/PostRouter"
 import ProfileRouter from "./src/routes/ProfileRouter"
+import AuthRouter from './src/routes/AuthRouter'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 dotenv.config();
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}))
 
-/* Routes */
+declare module "express-serve-static-core" {
+    interface Request {
+        profile?: { profileId: string};
+    }
+}
+
 app.use("/posts", PostRouter);
 app.use("/profiles", ProfileRouter);
+app.use("/auth", AuthRouter)
 
 export default app;

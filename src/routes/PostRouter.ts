d84dcabeab1f809ce42360f300ffbde68fe82628
Kawã Sousa <kawasousacontato@gmail.com>
2025-02-02 07:55:35 -0300

    :lock: Adiciona middleware de autenticação entre cada rota post

--- src/routes/PostRouter.ts
+++ src/routes/PostRouter.ts
@@ -1,23 +1,21 @@
 import { Router } from "express";
 import PostController from "../controllers/PostController";
-import { validateBody, validateParams } from "../utils/validate";
+import { validateBody, validateParams } from "../utils/schemaValidate";
 import { z } from 'zod'
+import jwt from '../utils/token'
 
 const router = Router();
 const postController: PostController = new PostController();
 
 const PostSchema = z.object({
     content: z.string().min(1),
-    profileId: z.string().uuid(),
-    isAdvanced: z.boolean()
+    username: z.string()
 })
 
-const PostIdSchema = z.object({
-    id: z.string().uuid()
-});
+const PostIdSchema = z.object({id: z.string().uuid()})
 
-router.get("/", postController.getAllPosts.bind(postController));
-router.post("/", validateBody(PostSchema), postController.createPost.bind(postController));
-router.delete("/:id", validateParams(PostIdSchema), postController.deletePost.bind(postController));
+router.get("/", jwt.validateToken, postController.getAllPosts.bind(postController));
+router.post("/", jwt.validateToken, validateBody(PostSchema), postController.createPost.bind(postController));
+router.delete("/:id", jwt.validateToken, validateParams(PostIdSchema), postController.deletePost.bind(postController));
 
 export default router;

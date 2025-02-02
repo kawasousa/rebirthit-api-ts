1e18914eb174ea0cadd4754a86985a34bfd2261f
Kawã Sousa <kawasousacontato@gmail.com>
2025-02-02 08:05:13 -0300

    :sparkles: feat: Adiciona Service de Profile com busca e exclusão de perfil

--- src/services/ProfileService.ts
+++ src/services/ProfileService.ts
@@ -1,23 +1,40 @@
 import prisma from "../../prisma/client";
-import { Profile } from '@prisma/client'
+import { ProfileDTO } from "../models/ProfileDTO";
 
 export default class ProfileService {
-    public async findAllProfiles() {
-        return prisma.profile.findMany();
+    public async findAllProfiles(): Promise<ProfileDTO[]> {
+        const profiles = await prisma.profile.findMany();
+        const profileDTOs: ProfileDTO[] = [];
+        
+        for(const profile of profiles){
+            const postsCount = await prisma.post.count({where: {profileId: profile.id}});
+            console.log(postsCount)
+
+            const profileDTO = new ProfileDTO(profile.username, profile.name, profile.icon, profile.role, postsCount);
+            profileDTOs.push(profileDTO);
+        }
+
+        return profileDTOs;
     }
 
-    public async createProfile(nickname: string, email: string, photo_url: string,
-        password: string, isActivated: boolean, isAdvanced: boolean): Promise<Profile> {
+    public async findProfileById(id: string): Promise<ProfileDTO | null> {
+        const profile = await prisma.profile.findUnique({
+            where: { id }
+        })
 
-        return prisma.profile.create({
-            data: {
-                nickname: nickname,
-                email: email,
-                photo_url: photo_url,
-                password: password,
-                isActivated: isActivated,
-                isAdvanced: isAdvanced
-            }
+        if(profile){
+            const postsCount = await prisma.post.count({where: {profileId: profile.id}});
+
+            const profileDIO: ProfileDTO = new ProfileDTO(profile.username, profile.name, profile.icon, profile.role, postsCount);
+            return profileDIO;
+        }
+
+        return null;
+    }
+    
+    public async deleteProfile(id: string) {
+        return prisma.profile.delete({
+            where: { id: id }
         })
     }
 }

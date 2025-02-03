1b2a8e9c8e13ad1effffa501e2d5fb2e018487cf
Kawã Sousa <kawasousacontato@gmail.com>
2025-02-03 14:29:36 -0300

    :recycle: refactor: Atualiza origem do cors para variável de ambiente

--- app.ts
+++ app.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.REBIRTHIT_FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
